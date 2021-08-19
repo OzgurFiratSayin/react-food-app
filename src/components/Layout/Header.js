348ad54c5a07f6fced175b3925adb2b65d629777
Ozgur Firat Sayin <ozgurfiratsayin@gmail.com>
2021-08-19 10:33:26 +0100

    Created header cart button

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,20 +1,22 @@
 import React from 'react';
 
+import HeaderCartButton from './HeaderCartButton';
+
 import headerImage from '../../assets/meals.jpg';
 import classes from './Header.module.css';
 
 const Header = () => {
   return (
     <React.Fragment>
-      <header className={classes.header} >
+      <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
-      <div className={classes['main-image']} >
+      <div className={classes['main-image']}>
         <img src={headerImage} className={classes.img} alt='Dinner table full of food'/>
       </div>
     </React.Fragment>
-  )
-}
+  );
+};
 
 export default Header;
