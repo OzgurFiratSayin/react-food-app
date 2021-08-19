b1a29a2fe5facf9159ecc114ef369bc9373c1680
Ozgur Firat Sayin <ozgurfiratsayin@gmail.com>
2021-08-19 11:01:08 +0100

    Create Meals and its sub components

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import MealsSummary from './MealsSummary';
+import MealsAvailable from './MealsAvailable';
+
+const Meals = (props) => {
+  return (
+    <React.Fragment>
+      <MealsSummary />
+      <MealsAvailable />
+    </React.Fragment>
+  );
+};
+
+export default Meals;
