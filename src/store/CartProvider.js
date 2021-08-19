95f9d5d964b0c32968166ee8e270024dba8aff04
Ozgur Firat Sayin <ozgurfiratsayin@gmail.com>
2021-08-19 17:05:37 +0100

    Cart reducer logic changed to display single line per distinct item

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -9,10 +9,26 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === 'ADD') {
-    const updatedItemsList = state.items.concat(action.item);
     const UpdatedTotalAmount = state.totalAmount + action.item.price + action.item.amount;
+    const existingItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    const existingItem = state.items[existingItemIndex];
+    let updatedItems;
+
+    if (existingItem) {
+      const updatedItem = {
+        ...existingItem,
+        amount: existingItem.amount + action.item.amount
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
+
     return {
-      items: updatedItemsList,
+      items: updatedItems,
       totalAmount: UpdatedTotalAmount
     };
   }
